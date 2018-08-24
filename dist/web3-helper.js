1396116de08636d1a973e6e25f4e927e1d13f8eb
agusputra <agus.putra99@gmail.com>
2018-08-24 17:21:23 +0700

    remove metamask detection

--- dist/web3-helper.js
+++ dist/web3-helper.js
@@ -2,92 +2,10 @@
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-var isCheckInstall = false;
-var isCheckLogin = false;
-var cbs = {
-  checkInstall: [],
-  checkLogin: []
-};
-
 var Metamask = {
-  onCheckInstall: onCheckInstall,
-  onCheckLogin: onCheckLogin,
-  checkMetamask: checkMetamask,
-  metamaskInstalled: metamaskInstalled,
-  metamaskLogin: metamaskLogin,
   deployContract: deployContract,
   submitTx: submitTx
-
-  // This method will checking metamask installation first.
-  // And call the callback when metamask installed.
-  // The callback will be called immediately if checking process already done.
-};function onCheckInstall(cb) {
-  if (isCheckInstall) {
-    return cb();
-  }
-  cbs.checkInstall.push(cb);
-}
-
-// This method will checking if user login to metamask first.
-// And call the callback if user login.
-// The callback will be called immediately if checking process already done.
-function onCheckLogin(cb) {
-  if (isCheckLogin) {
-    return cb();
-  }
-  cbs.checkLogin.push(cb);
-}
-
-// Trigger metamask checking for <retry> times.
-// This method will also call callback after checking process done.
-function checkMetamask(retry) {
-  setTimeout(checkInstall, 0, retry);
-  setTimeout(checkLogin, 0, retry);
-
-  function checkInstall(retry) {
-    retry--;
-
-    if (retry && !metamaskInstalled()) {
-      return setTimeout(checkInstall, 500, retry);
-    }
-    isCheckInstall = true;
-    cbs.checkInstall.forEach(function (cb) {
-      return setTimeout(cb);
-    });
-    cbs.checkInstall = [];
-  }
-
-  function checkLogin(retry) {
-    retry--;
-
-    if (retry && !metamaskLogin()) {
-      return setTimeout(checkLogin, 500, retry);
-    }
-    isCheckLogin = true;
-    cbs.checkLogin.forEach(function (cb) {
-      return setTimeout(cb);
-    });
-    cbs.checkLogin = [];
-  }
-}
-
-// Check if metamask installed or not
-function metamaskInstalled() {
-  // The line below temporary because it's not the right way to use MetaMask.
-  // (The right way to use MetaMask: https://github.com/MetaMask/faq/blob/master/detecting_metamask.md#web3-deprecation)
-  if (web3 && web3.currentProvider.isMetaMask) {
-    return true;
-  }
-  return false;
-}
-
-// Check if user login to metamask or not
-function metamaskLogin() {
-  if (metamaskInstalled() && web3.eth.defaultAccount) {
-    return true;
-  }
-  return false;
-}
+};
 
 function deployContract(Contract, byteCode, params, options, cb) {
   var _Contract$new;
